ea7341bd1baccf4e26c9730574a89af0013a7df4
Stefan Markovic <stefan@janeasystems.com>
2021-11-05 18:04:26 +0100

    Init 2

---
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI.WinUI3/Interop.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.InteropServices;
+
+using WinCore = Windows.UI.Core;
+
+namespace Microsoft.PowerToys.Settings.UI.WinUI3
+{
+    internal static class Interop
+    {
+        public static ICoreWindowInterop GetInterop(this WinCore.CoreWindow @this)
+        {
+            var unkIntPtr = Marshal.GetIUnknownForObject(@this);
+            try
+            {
+                var interopObj = Marshal.GetTypedObjectForIUnknown(unkIntPtr, typeof(ICoreWindowInterop)) as ICoreWindowInterop;
+                return interopObj;
+            }
+            finally
+            {
+                Marshal.Release(unkIntPtr);
+                unkIntPtr = System.IntPtr.Zero;
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "Interop naming consistency")]
+        public const int SW_HIDE = 0;
+    }
+}
