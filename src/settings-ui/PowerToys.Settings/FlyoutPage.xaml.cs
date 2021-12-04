dfd7d6cbe85e3a54099eb67c8c8b4144f48cda31
niels9001 <niels.laute@live.nl>
2021-12-04 14:15:39 +0100

    Create flyoutpage

---
+++ src/settings-ui/PowerToys.Settings/FlyoutPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PowerToys.Settings
+{
+    /// <summary>
+    /// Interaction logic for FlyoutPage.xaml
+    /// </summary>
+    public partial class FlyoutPage : Window
+    {
+        public FlyoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
